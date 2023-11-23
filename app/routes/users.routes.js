afcfddb40999e1d71566a056c8d4eedcb92ade40
SinekaRamu <sineka2001cud@gmail.com>
2023-11-23 22:46:04 +0530

    update user data

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -4,14 +4,18 @@ const router = express.Router();
 const {
     addUserController,
     loginController,
+    updateUserController,
 } = require ('../controllers/accountUser.controller');
 const { validate } = require('../middlewares/validate.middleware');
-const { signUpSchema } = require('../validation/authentication.schema');
+const { signUpSchema, updateUserSchema } = require('../validation/authentication.schema');
 
 //CREATE USER ACCOUNT
 router.post("/signup", validate(signUpSchema), addUserController);
 
 //LOGIN
 router.post("/login", loginController);
+
+//UPDATE USER DATA
+router.put("/:id", validate(updateUserSchema), updateUserController);
 
 module.exports = router;
